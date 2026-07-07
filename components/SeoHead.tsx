dc75763e34c2993ddda7d08fcf8e0e248247c76a
Watcher <watchereth10@gmail.com>
2026-07-07 12:46:04 -0700

    og fix

--- components/SeoHead.tsx
+++ components/SeoHead.tsx
@@ -8,7 +8,10 @@ type SeoHeadProps = {
 
 const siteName = "Lola";
 const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.lolaliving.de";
-const ogImagePath = "/Lola.jpg";
+const ogImagePath = "/LolaOg.png";
+const ogImageAlt = "LOLA Wohnhaus in Berlin-Wilmersdorf";
+const ogImageWidth = "1450";
+const ogImageHeight = "1085";
 
 export function SeoHead({
   title,
@@ -30,8 +33,10 @@ export function SeoHead({
       />
       <meta key="robots" name="robots" content="index,follow" />
       <link key="canonical" rel="canonical" href={canonicalUrl} />
+      <link key="image_src" rel="image_src" href={ogImageUrl} />
 
       <meta key="og:type" property="og:type" content="website" />
+      <meta key="og:locale" property="og:locale" content="de_DE" />
       <meta key="og:site_name" property="og:site_name" content={siteName} />
       <meta key="og:title" property="og:title" content={fullTitle} />
       <meta
@@ -41,18 +46,27 @@ export function SeoHead({
       />
       <meta key="og:url" property="og:url" content={canonicalUrl} />
       <meta key="og:image" property="og:image" content={ogImageUrl} />
+      <meta key="og:image:url" property="og:image:url" content={ogImageUrl} />
       <meta
         key="og:image:secure_url"
         property="og:image:secure_url"
         content={ogImageUrl}
       />
-      <meta key="og:image:type" property="og:image:type" content="image/jpeg" />
-      <meta key="og:image:width" property="og:image:width" content="2400" />
-      <meta key="og:image:height" property="og:image:height" content="1792" />
+      <meta key="og:image:type" property="og:image:type" content="image/png" />
+      <meta
+        key="og:image:width"
+        property="og:image:width"
+        content={ogImageWidth}
+      />
+      <meta
+        key="og:image:height"
+        property="og:image:height"
+        content={ogImageHeight}
+      />
       <meta
         key="og:image:alt"
         property="og:image:alt"
-        content="Lola in Berlin-Wilmersdorf"
+        content={ogImageAlt}
       />
 
       <meta
@@ -61,6 +75,7 @@ export function SeoHead({
         content="summary_large_image"
       />
       <meta key="twitter:title" name="twitter:title" content={fullTitle} />
+      <meta key="twitter:url" name="twitter:url" content={canonicalUrl} />
       <meta
         key="twitter:description"
         name="twitter:description"
@@ -68,9 +83,14 @@ export function SeoHead({
       />
       <meta key="twitter:image" name="twitter:image" content={ogImageUrl} />
       <meta
+        key="twitter:image:src"
+        name="twitter:image:src"
+        content={ogImageUrl}
+      />
+      <meta
         key="twitter:image:alt"
         name="twitter:image:alt"
-        content="Lola in Berlin-Wilmersdorf"
+        content={ogImageAlt}
       />
     </Head>
   );
